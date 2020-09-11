56d9cb3a71c270c077f0adc0db46cfaa0a2d7769
Eliud Berlin <info@eliud.io>
2020-09-11 09:39:08 +0200

    fixed minkey

--- lib/fundamentals_web_package.dart
+++ lib/fundamentals_web_package.dart
@@ -2,7 +2,7 @@ import 'fundamentals_package.dart';
 import 'package:eliud_pkg_fundamentals/model/abstract_repository_singleton.dart';
 import 'package:eliud_pkg_fundamentals/model/js_repository_singleton.dart';
 
-abstract class FundamentalsWebPlugin extends FundamentalsPlugin {
+class FundamentalsWebPlugin extends FundamentalsPlugin {
   @override
   void initRepository(String appID) {
     AbstractRepositorySingleton.singleton = JsRepositorySingleton(appID);
