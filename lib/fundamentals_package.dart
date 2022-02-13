3585a6b48f13bf82d0b3bd66f4be1cea33f63fd7
Eliud <58745656+eliudio@users.noreply.github.com>
2022-02-13 20:58:14 +0000

    Bugfixes

--- lib/fundamentals_package.dart
+++ lib/fundamentals_package.dart
@@ -1,4 +1,5 @@
 import 'package:eliud_core/core/blocs/access/access_bloc.dart';
+import 'package:eliud_core/core/wizards/registry/registry.dart';
 import 'package:eliud_core/model/access_model.dart';
 import 'package:eliud_core/model/app_model.dart';
 import 'package:eliud_core/model/member_model.dart';
@@ -6,7 +7,6 @@ import 'package:eliud_core/package/package.dart';
 import 'package:eliud_pkg_fundamentals/model/abstract_repository_singleton.dart';
 import 'package:eliud_pkg_fundamentals/model/component_registry.dart';
 import 'package:eliud_pkg_fundamentals/model/repository_singleton.dart';
-import 'package:flutter_bloc/src/bloc_provider.dart';
 
 abstract class FundamentalsPackage extends Package {
   FundamentalsPackage() : super('eliud_pkg_fundamentals');
